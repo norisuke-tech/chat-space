8ec8fe5d18fde92bba19a975d7b8ecf3e7761152
norisuke-tech <happy.sou.ann@gmail.com>
2020-02-18 13:44:00 +0900

    chats_doki

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="chat-main__group">
+           <div class="chat-main__group--name">
+             ${message.user_name}
+           </div>
+           <div class="chat-main__group--name--time">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="chat-main__message--info">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+        <div class="chat-main__group">
+          <div class="chat-main__group--name">
+            ${message.user_name}
+          </div>
+          <div class="chat-main__group--name--time">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="chat-main__message--info">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+    return html;
+   };
+ }
+$('#new_message').on('submit', function(e){
+  console.log("success")
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.chat-main').append(html);    
+    $('form')[0].reset();
+    $('.chat-main').animate({ scrollTop: $('.chat-main')[0].scrollHeight});
+    $('.submit-btn').prop('disabled', false);
+  })
+  .fail(function(){
+    alert('error');
+  })
+})
+});
